79a30926ec09d7f63654eb425c8e2b4a19ea53a9
hzl <1095735209@qq.com>
2019-08-21 15:32:44 +0800

    表情功能

---
+++ GuiPackage/Expression.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace chat2._0.GuiPackage
+{
+    public partial class Expression : UserControl
+    {
+        List<Image> _images = new List<Image>();
+
+        private void LoadImages()
+        {
+            DirectoryInfo dir = new DirectoryInfo("../Debug/Faces");
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                if (String.Compare(file.Extension, ".gif", true) == 0)
+                {
+                    _images.Add(Image.FromFile(file.FullName));
+                }
+            }
+        }
+
+        public EventHandler OnSelectExpression;
+        public static Expression Instance = new Expression();
+
+        public Expression()
+        {
+            InitializeComponent();
+            LoadImages();
+            LoadForm();
+        }
+
+        public void LoadForm()
+        {
+            int idx = 0;
+            foreach (Image img in _images)
+            {
+                if (img != null)
+                {
+                    PictureBox pb = new PictureBox();
+                    pb.Size = new Size(40, 30);
+                    pb.Location = new Point(40 * (idx % 10), 30 * (idx / 10));
+                    pb.Image = img;
+                    pb.Click += OnPictureBoxClick;
+                    idx++;
+                    this.Controls.Add(pb);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            this.Show();
+        }
+
+        private void OnPictureBoxClick(object sender, EventArgs e)
+        {
+            if (OnSelectExpression != null)
+            {
+                OnSelectExpression(sender, e);
+            }
+        }
+    }
+}
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -63,6 +63,26 @@ namespace chat2._0.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap _0019 {
+            get {
+                object obj = ResourceManager.GetObject("0019", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap _00191 {
+            get {
+                object obj = ResourceManager.GetObject("00191", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap e {
             get {
                 object obj = ResourceManager.GetObject("e", resourceCulture);
