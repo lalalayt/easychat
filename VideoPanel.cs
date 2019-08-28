255d51ab2754b504c9702ae809aba253f1281a11
hzl <1095735209@qq.com>
2019-08-28 11:33:11 +0800

    视频聊天

---
+++ VideoPanel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chat2._0
+{
+    public partial class VideoPanel : UserControl
+    {
+        public VideoPanel()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);//调整大小时重绘
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);// 双缓冲
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);// 禁止擦除背景.
+            this.SetStyle(ControlStyles.UserPaint, true);//自行绘制            
+            this.UpdateStyles();
+        }
+    }
+}
